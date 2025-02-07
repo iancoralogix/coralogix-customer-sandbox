725c3dd81cd7ad4b9c16bcbb4de895182d69407c
iancoralogix <ian.bowers@coralogix.com>
2025-02-07 16:49:38 -0500

    init

--- run.sh
+++ run.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-cd terraform
+pushd terraform
 terraform init
 terraform apply --auto-approve
+
+CORALOGIX_DOMAIN="${CORALOGIX_DOMAIN:-cx498.coralogix.com}"
+CLUSTER_NAME="${CLUSTER_NAME:-otel-demo}"
+
+if [ -n "${CORALOGIX_PRIVATE_KEY}" ]; then
+  helm repo add coralogix https://cgx.jfrog.io/artifactory/coralogix-charts-virtual
+  helm repo update
+
+  kubectl create secret generic coralogix-keys --from-literal=PRIVATE_KEY="${CORALOGIX_PRIVATE_KEY}"
+
+  helm upgrade --install otel-coralogix-integration coralogix/otel-integration \
+    --version=0.0.143 \
+    --render-subchart-notes \
+    --set global.domain="${CORALOGIX_DOMAIN}" \
+    --set global.clusterName="${CLUSTER_NAME}"
+else
+  echo "Error: Run \'export CORALOGIX_PRIVATE_KEY=<your_private_key>\'"
+  exit 1
+fi
--- terraform/modules/opentelemetry-demo-infrastructure/main.tf
+++ terraform/modules/opentelemetry-demo-infrastructure/main.tf
@@ -20,15 +20,52 @@ module "vpc" {
   public_subnets  = ["10.0.8.0/22", "10.0.12.0/22"]
 }
 
-module "eks" {
-  source = "git@github.com:terraform-aws-modules/terraform-aws-eks.git?ref=v20.33.1"
-  create = true
-
-  cluster_name                             = "${data.aws_default_tags.current.tags["Name"]}-eks"
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
-
-  vpc_id                   = module.vpc.vpc_id
-  control_plane_subnet_ids = module.vpc.private_subnets
-  subnet_ids               = module.vpc.private_subnets
-}
+#module "eks" {
+#  source = "git@github.com:terraform-aws-modules/terraform-aws-eks.git?ref=v20.33.1"
+#  create = true
+#
+#  cluster_name                             = "${data.aws_default_tags.current.tags["Name"]}-eks"
+#  cluster_endpoint_public_access           = true
+#  enable_cluster_creator_admin_permissions = true
+#
+#  cluster_compute_config = {
+#    enabled    = true
+#    node_pools = ["general-purpose"]
+#  }
+#
+#  vpc_id                   = module.vpc.vpc_id
+#  control_plane_subnet_ids = module.vpc.private_subnets
+#  subnet_ids               = module.vpc.private_subnets
+#
+#  eks_managed_node_groups = {
+#    example = {
+#      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+#      instance_types = ["m6i.large"]
+#
+#      min_size = 2
+#      max_size = 5
+#      # This value is ignored after the initial creation
+#      # https://github.com/bryantbiggs/eks-desired-size-hack
+#      desired_size = 2
+#
+#      # This is not required - demonstrates how to pass additional configuration to nodeadm
+#      # Ref https://awslabs.github.io/amazon-eks-ami/nodeadm/doc/api/
+#      cloudinit_pre_nodeadm = [
+#        {
+#          content_type = "application/node.eks.aws"
+#          content      = <<-EOT
+#            ---
+#            apiVersion: node.eks.aws/v1alpha1
+#            kind: NodeConfig
+#            spec:
+#              kubelet:
+#                config:
+#                  shutdownGracePeriod: 30s
+#                  featureGates:
+#                    DisableKubeletCloudCredentialProviders: true
+#          EOT
+#        }
+#      ]
+#    }
+#  }
+#}
